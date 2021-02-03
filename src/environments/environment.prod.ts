bd3574d08a2bc82795c2a2283d243393b1967186
Daniil Slobin <danikslobin@gmail.com>
2021-02-03 14:44:46 +0300

    Remove production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,3 @@
 export const environment = {
   production: true,
-  authBaseUrl: 'http://ec2-18-184-164-153.eu-central-1.compute.amazonaws.com:8080/api/auth',
-  cartBaseUrl: 'http://ec2-18-184-164-153.eu-central-1.compute.amazonaws.com:8080/api/cart',
-  certificateBaseUrl: 'http://ec2-18-184-164-153.eu-central-1.compute.amazonaws.com:8080/api/certificates',
-  orderBaseUrl: 'http://ec2-18-184-164-153.eu-central-1.compute.amazonaws.com:8080/api/orders',
-  tagBaseUrl: 'http://ec2-18-184-164-153.eu-central-1.compute.amazonaws.com:8080/api/tags'
 };
